548174335c79dfdb36997cc11de2eab7957494d0
writethesky <writethesky@163.com>
2016-10-26 15:27:44 +0800

    完善readme

--- README.md
+++ README.md
@@ -1,1 +1,125 @@
 # application.js
+
+#依赖
+
+```jquery```
+
+#使用方法
+
+引入 ```<script src="application.js"></script>```
+
+##使用api接口
+
+设置api服务器地址
+
+```window.api.host = "http://xxx.xxx.xxx/";```
+
+设置api版本
+
+```window.api.version = 'v1';```
+
+api请求分为两种
+
+发送get请求
+
+```
+window.api.get('接口名称', 回调函数);
+```
+如
+
+```
+window.api.get('user/get_name', function(data){
+    //todo 接口请求成功后的后续操作
+});
+```
+
+如上代码将发送如下请求
+
+```
+Request URL:http://xxx.xxx.xxx/v1/user/get_name
+Request Method:GET
+Host:xxx.xxx.xxx
+```
+
+发送post请求
+
+```window.api.post(接口名称, 参数, 回调函数);```
+
+如
+
+```
+window.api.post('user/set_name', {name: 'testname'}, function(){
+	//todo 接口请求成功后的后续操作
+});
+```
+
+如上代码将发送如下请求
+
+```
+Request URL:http://xxx.xxx.xxx/v1/user/get_name
+Request Method:POST
+Host:xxx.xxx.xxx
+Form Data:
+name:testname
+```
+
+
+##使用自动载入
+
+自动载入包括html、css、js。自动载入默认使用的还是```<a>```标签，但对```<a>```标签的机制进行了调整，点击```<a>```标签不再是同步页面跳转，而是自动进行异步页面加载
+
+对```<a>```标签进行了属性扩展，现增加属性如下
+
+```data-href``` 需要异步加载的html文件地址
+
+```data-id```    容器id，将会把异步加载的内容渲染到指定的容器中
+
+```data-css``` 需要异步加载的css文件地址
+
+```data-js``` 需要异步加载的js文件地址
+
+```data-obj``` 需要夸页面传输的数据
+
+使用示例
+```
+<a 
+	href="javascript:void(0)"
+	data-id="content"
+	data-href="register.html"
+	data-css="register.css"
+	data-js="register.js"
+	data-obj="{user_id: 111}"
+
+>这是一个超连接</a>
+```
+多css自动载入
+```data-css="css1.css|css2.css|css3.css"```
+
+多js自动载入
+```data-js="js1.js|js2.js|js3.js"```
+
+
+###构造函数
+
+对于异步引入的js文件，会自动运行起内部的构造函数
+
+*建议每个js文件都要闭包，以免各个js文件中产生命名冲入
+
+如
+
+```
+(function(w){
+    //todo 这里开始编辑js代码
+	var file_name = 'brand.js'; //当前js文件的文件名
+	// 构造函数
+	w[file_name] = function(){
+		//todo 需要初始化的逻辑
+	}
+	w[file_name]();
+	
+	
+	// todo 其它的代码逻辑
+}(window));
+```
+
+
---
+++ application.js
@@ -0,0 +1,253 @@
+/**
+ * 
+ */
+(function(w){
+	w.api = {
+		host: "http://139.224.54.221:8002/",
+//		host: "http://192.168.199.10:8002/",
+		version: "v1",
+	}
+	w.api.url = w.api.host + w.api.version;
+	w.api.get = function(url, callback){
+
+		
+		$.ajax({
+			type:"get",
+			url:w.api.url + "/" + url,
+			dataType: 'json',
+			async:true,
+			xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            success: function(data){
+            	callback(data);
+            }
+		});
+	}
+	w.api.post = function(url,arr,callback){
+		$.ajax({
+			type:"post",
+			data: arr,
+			url:w.api.url + "/" + url,
+			dataType: 'json',
+			async:true,
+			xhrFields: {
+                withCredentials: true
+            },
+            crossDomain: true,
+            success: function(data){
+            	callback(data);
+            }
+		});
+	}
+	
+	
+
+
+
+	w.alert.alert_message = function(msg, m_width, m_height, m_loop_time, m_type){
+		
+		m_width = m_width ? m_width : '100%';
+		m_height = m_height ? m_height : '2rem';
+		m_loop_time = m_loop_time ? m_loop_time : 500;
+		m_id = 'alert_' + (new Date()).getTime();
+		
+		b_height = $(window).height();
+		var l_top = b_height / 2;
+
+
+		
+		var m_background_color = "rgb(255, 116, 116)";
+		switch(m_type){
+			case 'error':
+				m_background_color = "rgb(255, 116, 116)";
+				break;
+			case 'success':
+				m_background_color = "#65e26e";
+				break;
+			default:
+				m_background_color = "rgb(255, 116, 116)";
+				break;
+		}
+		var dom = $("<div />").css({
+			width: m_width,
+			height: m_height,
+			background: m_background_color,
+			position: 'fixed',
+			color: "#fff",
+			textAlign: 'center',
+			lineHeight: m_height,
+			top: b_height
+		}).attr('id', m_id).text(msg);
+		
+		$("body").append(dom);
+		
+		
+		dom.animate({'top': l_top}, m_loop_time);
+		
+		
+		setTimeout(function(){
+			dom.remove();
+		}, m_loop_time + 2000);
+
+		
+	}
+	
+	
+	w.alert.error = function(msg, m_width, m_height, m_loop_time){
+		w.alert.alert_message(msg, m_width, m_height, m_loop_time, 'error');
+	}
+	w.alert.success = function(msg, m_width, m_height, m_loop_time){
+		w.alert.alert_message(msg, m_width, m_height, m_loop_time, 'success');
+	}
+
+	
+	
+	$("html").on("click", "a", load_page);
+	
+	function load_page(){
+		
+		var dataHref = $(this).attr("data-href");
+		var dataId = $(this).attr("data-id");
+		var dataCss = $(this).attr("data-css");
+		var dataJs = $(this).attr("data-js");
+		
+		var dataObj = $(this).attr("data-obj");
+		if(dataObj){
+			dataObj = dataObj.replace(/'/g, '"');
+			
+			(dataObj = $.parseJSON(dataObj));
+			w.dataObj = dataObj;
+		}
+		dataCss = dataCss.split("|");
+		dataJs = dataJs.split("|");
+		
+		dataHref && load.html(dataHref, dataId, function(){
+			
+			dataCss && load.css(dataCss, function(){
+				
+				dataJs && load.js(dataJs);
+			});
+		});
+	}
+	
+	function load(){
+		this.html = function(url, id, callback){
+			$.get(url, function(data){
+				$("#" + id).html(data);
+				callback && callback();
+			});
+		}
+		
+		this.css = function(arr, callback){
+			
+			for(i in arr){
+				
+				var href = 'public/css/' + arr[i];
+				var head = document.getElementsByTagName('HEAD').item(0);
+				var is_run = true;
+				$("link").each(function(){
+		
+					var tmp = window.location.href.split("/");
+					var end_length = tmp[tmp.length - 1].length;
+		
+					var complete_href = (window.location.href.substr(0, window.location.href.length - end_length) + href);
+
+					if(complete_href == this.href){
+						is_run = false;
+					}
+				});
+				
+				if(is_run){
+					var style = document.createElement('link');
+					style.href = href;
+					style.rel = 'stylesheet';
+					style.type = 'text/css';
+					head.appendChild(style);
+					
+				}
+				
+			}
+			callback && callback();
+		}
+		
+		this.js = function(arr, callback){
+//			console.log(arr);
+			for(i in arr){
+				var src = 'public/js/' + arr[i];
+				var body = document.getElementsByTagName('body').item(0);
+				var is_run = true;
+				$("script").each(function(){
+		
+					var tmp = window.location.href.split("/");
+					var end_length = tmp[tmp.length - 1].length;
+		
+					var complete_src = (window.location.href.substr(0, window.location.href.length - end_length) + src);
+
+					if(complete_src == this.src){
+						is_run = false;
+						
+					}
+				});
+				if(is_run){				
+					var script = document.createElement('script');
+					script.src = src;
+					script.type = 'text/javascript';
+					body.appendChild(script);
+				}else{
+					var js_name = arr[i];
+//					console.log(js_name);
+					w[js_name] && w[js_name]();
+				}
+			}
+			callback && callback();
+		}
+	}
+	var load = new load();
+	
+	
+	$('a').each(function(){
+		if($(this).attr("checked")){
+			load_page.call(this);
+		}
+	});
+	
+	//封装一个获取 时间格式为 yyyy/mm/dd  的方法
+	w.getFormatDate = function(dateTmie){
+		var myDate = new Date(dateTmie*1000);
+		var yy = myDate.getFullYear();
+		var mm = myDate.getMonth()+1;
+		var dd = myDate.getDate();
+		return yy + "/" + getTwo(mm) + "/" + getTwo(dd);
+		//获取两位数
+		function getTwo(num){
+			if(num<10){
+				return "0" + num;
+			}else{
+				return num;
+			}
+		}
+	}
+//导航切换 背景变色
+	$(".left_bar li").click(function(){
+		$(".left_bar li").css("background","transparent")
+		$(this).css("background","#00bbe9");
+	});
+	
+	//判断登陆  跳转登录页
+	function check_login_status(){
+		w.api.get('index/index', function(data){
+			if(data.status == 0){
+				window.location.href = "login.html";
+			}
+		});
+		setTimeout(check_login_status, 10000);
+	}
+	var url = (window.location.href.split("/"));
+	url = url[url.length - 1];
+	(url == 'login.html') || check_login_status();
+
+	
+	
+}(window));
